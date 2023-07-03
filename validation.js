39b68ee5ef5b4cd361d90f2714bae5807d200b27
jniyonkuru <niyonkurujacques@gmail.com>
2023-07-04 00:52:27 +0200

    validate the form with regx

--- validation.js
+++ validation.js
@@ -1,1 +1,25 @@
 // validation script here
+const patterns={
+    telephone:/^\d{11}$/,
+    username:/^[a-z\d]{5,12}$/,
+    password:/^[\w@-]{8,20}$/i,
+    slug:/^[a-z\d-]{8,20}$/,
+    email:/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+}
+
+
+function validate (field,regex){
+    if(regex.test(field.value)){
+        field.className='valid'; 
+}else{
+    field.className='invalid';
+}
+
+}
+ const inputs =document.querySelectorAll("input");
+ inputs.forEach(input=> {
+    input.addEventListener("keyup",(e)=>{
+        validate(e.target,patterns[e.target.attributes.name.value])
+    })
+    
+});
